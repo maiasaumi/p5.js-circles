7108df2fffb24d62d980fd3cb9e181e9cabb2e4c
Mai <dctmm222222@gmail.com>
2021-02-07 23:21:26 +0900

    Practices some p5.js

---
+++ script.js
@@ -0,0 +1,14 @@
+let x = 0;
+
+function setup() {
+  createCanvas(480, 240);
+  background("skyblue");
+
+  rect(width / 2, height / 2, 150, 100);
+}
+
+/*function draw() {
+  background(0);
+  rect(x, 40, 50, 50);
+  x++;
+}*/
