cd59cb6a15508465a3e674cde481f66a9b412dfb
Mai <dctmm222222@gmail.com>
2021-03-29 15:59:08 +0900

    Circle art done

--- script.js
+++ script.js
@@ -1,128 +1,15 @@
-let snowflakes = [];
-const canvasColor = "skyblue";
-//let img;
-
-function preload() {
-  backimage = loadImage("assets/merry-go-round.jpg");
-}
-
-//Process to be executed only once at the beginning.
 function setup() {
-  createCanvas(1000, 1000);
-  background(canvasColor);
-  //Fill in the shape
-  //fill();
-
-  //Do not draw contour lines.
-  //noStroke();
-  //How many times to refresh per second.
-  frameRate(30);
-}
-
-//Process to be executed repeatedly
-function draw() {
-  //show image
-  image(backimage, 0, 0);
-  //Update time
-  let time = frameCount / 60;
-}
-
-class Snowflake {
-  constructor() {
-    //Initialization of coordinates
-    this.x = 0;
-    this.y = random(-50, 0);
-    this.initialAngle = random(0, 2 * PI);
-    this.size = random(3, 10);
-    this.radius = sqrt(random(pow(width / 2, 2)));
-  }
-
-  update(time) {
-    //x coordinate
-    let speedOfAngle = 0.6;
-    let angle = speedOfAngle * time + this.initialAngle;
-    this.x = width / 2 + this.radius * sin(angle);
-
-    //y coordinate
-    this.y += pow(this.size, 0.5);
-
-    //eliminate snowflakes at the bottom
-    if (this.y > height) {
-      let index = snowflakes.indexOf(this);
-      snowflakes.splice(index, 1);
-    }
-  }
-
-  display() {
-    // Top-left corner of the img is at (this.x, this.y)
-    // Width and height are this.size x this.size
-    image(snowA, this.x, this.y, this.size, this.size);
-  }
-}
-
-__________________________________________________________;
-
-let snowflakes = [];
-
-function preload() {
-  backimage = loadImage("assets/merry-go-round.jpg");
-  snowA = loadImage("assets/snowC.png");
-}
-
-function setup() {
-  createCanvas(426, 525);
+  createCanvas(windowWidth, windowHeight);
   noStroke();
-  fill(255);
-  //How many times to refresh per second.
-  frameRate(30);
-}
 
-function draw() {
-  image(backimage, 0, 0);
-  let time = frameCount / 60; // 更新時間（frameCountを60で割った数字）
-
-  // フレームごとに、乱数分の雪片を作成してsnowflakes配列に入れる
-  for (let i = 0; i < random(3); i++) {
-    snowflakes.push(new Snowflake());
-  }
-
-  for (let flake of snowflakes) {
-    flake.update(time); // 雪片の位置を更新する
-    flake.display(); // 雪片を描く
+  for (let i = 0; i < 100; i++) {
+    fill(random(0), random(250), random(250), random(250));
+    ellipse(random(windowWidth), random(windowHeight), random(100));
   }
 }
 
-class Snowflake {
-  constructor() {
-    // 座標の初期化
-    this.posX = 0; // 0で初期化
-    this.posY = random(-50, 0); // -50から0の間の数値で初期化
-    this.initialAngle = random(0, 2 * PI); // 初期の角度
-    this.size = random(3, 10); // 大きさ(2-5の間の乱数)
-
-    // 雪片の螺旋の半径
-    // 半径の2乗を最大値に生成した乱数の平方根を設定
-    // （雪片が一斉に広がるようにするため）
-    this.radius = sqrt(random(pow(width / 2, 2)));
-  }
-
-  update(time) {
-    // x座標：円に沿って動く
-    let w = 0.6; // 角度のスピード
-    let angle = w * time + this.initialAngle;
-    this.posX = width / 2 + this.radius * sin(angle);
-
-    // y座標：異なるサイズの雪片はわずかに異なるy速度で落下する
-    this.posY += pow(this.size, 0.5);
-
-    // 画面の最後にきた雪片を削除
-    if (this.posY > height) {
-      let index = snowflakes.indexOf(this);
-      snowflakes.splice(index, 1);
-    }
-  }
-
-  display() {
-    image(snowA, this.posX, this.posY, this.size, this.size);
-  }
-}
+//Step 1: Write a setup function
+//Step 2: Create canvas
+//Step 3: Create 500 circles with for loop
+//Step 4: Set up the color using fill()
+//Step 5: Draw actual circles with color using ellipse() fuction
